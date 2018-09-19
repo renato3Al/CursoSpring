432b5a794eadcfa683729b4ecee288c63ec7f67e
renato3Al <renato3.al@gmail.com>
2018-09-19 00:43:18 -0300

    Endpoint /pedidos/{id} incluido

---
+++ src/main/java/com/example/cursospring/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.example.cursospring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursospring.domain.Pedido;
+import com.example.cursospring.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
