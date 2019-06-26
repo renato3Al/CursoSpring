b5238ba6fee92aceaa424560c287c9b19c879b40
renato3Al <renato3.al@gmail.com>
2019-06-26 00:11:15 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/example/cursospring/dto/ClienteDTO.java
+++ src/main/java/com/example/cursospring/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.example.cursospring.domain.Cliente;
+import com.example.cursospring.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
