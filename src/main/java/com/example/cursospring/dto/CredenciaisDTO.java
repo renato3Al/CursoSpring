3eace1a73729904e37e2263a578075de173f6087
renato3Al <renato3.al@gmail.com>
2019-07-17 19:00:39 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/example/cursospring/dto/CredenciaisDTO.java
@@ -0,0 +1,33 @@
+package com.example.cursospring.dto;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	private String senha;
+	
+	public CredenciaisDTO() {
+		
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/cursospring/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.cursospring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.cursospring.domain.Cliente;
+import com.example.cursospring.repositories.ClienteRepository;
+import com.example.cursospring.security.UserSpringSecurity;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	 @Override
+	    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		 Cliente cli = repo.findByEmail(email);
+		 if (cli == null) {
+	            throw new UsernameNotFoundException(email);
+	        }
+	        return new UserSpringSecurity(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	    }
+}
