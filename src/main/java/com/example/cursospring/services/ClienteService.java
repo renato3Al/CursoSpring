349abde46cde26326bde2cb38f3015ba2d211f31
renato3Al <renato3.al@gmail.com>
2019-07-17 11:54:26 -0300

    Adicionando senha a Cliente

--- src/main/java/com/example/cursospring/services/ClienteService.java
+++ src/main/java/com/example/cursospring/services/ClienteService.java
@@ -7,6 +7,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +30,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
@@ -79,17 +83,18 @@ public class ClienteService {
 	}
 	
     public Cliente fromDTO(ClienteDTO objDto) {
-    	return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    	return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
     }
 	
-    public Cliente fromDTO(ClienteNewDTO objDto) {
-    	Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
-    	Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
-    	Endereco end = new Endereco(null, objDto.getLogadouro(), objDto.getNumero(), objDto.getComplemento(), 
-    			objDto.getBairro(), objDto.getCep(), cli, cid);
-    	
-    	cli.getEnderecos().add(end);
-    	cli.getTelefones().add(objDto.getTelefone1());
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), passwordEncoder.encode(objDto.getSenha()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogadouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
     	
     	if (objDto.getTelefone2() != null) {
         	cli.getTelefones().add(objDto.getTelefone2());
--- src/main/java/com/example/cursospring/services/DBService.java
+++ src/main/java/com/example/cursospring/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.cursospring.domain.Categoria;
@@ -51,6 +52,9 @@ public class DBService {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	public void instantiateTestDatabase() throws ParseException {
 		
@@ -113,7 +117,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Renato Sousa", "shinobby_19@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Renato Sousa", "shinobby_19@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA, passwordEncoder.encode("th3b3stof"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
